8a2cad5974f457c49b4e781341a0c8f8491d6f66
Jacob Blakely <jacob@codekirei.com>
2016-03-09 16:58:33 -0800

    add base logic and todo

---
+++ index.js
@@ -0,0 +1,15 @@
+const postcss = require('postcss')
+
+module.exports = postcss.plugin('postcss-beautify', () => css =>
+  css.walkRules(rule => {
+    // one selector per line
+    if (rule.selector.indexOf(', ') >= 0) {
+      rule.selector = rule.selector.replace(/, /g, ',\n')
+    }
+
+    // empty line between rules
+    if (rule.raws.before !== '') rule.raws.before = '\n\n'
+  })
+)
+
+/* eslint no-param-reassign: [2, {"props": false}] */
